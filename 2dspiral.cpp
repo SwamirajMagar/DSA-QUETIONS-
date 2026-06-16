b5829ac6483fc574e7eca8157aa4c41afb320b4e
SwamirajMagar <swamirajmagar8@gmail.com>
2026-06-16 23:51:10 +0530

    2nd prblm

---
+++ 2dspiral.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void spiralform(vector<vector<int>> &arr, int cols, int rows)
+{
+
+    int count = 0;
+    int total = cols * rows;
+
+    int startcoln = 0;
+    int startrow = 0;
+    int endcoln = cols -1 ;
+    int endrow = rows -1 ;
+
+    while (count < total)
+    {
+
+        for (int col = startcoln ; count < total && col < endcoln; col++)
+        {
+            cout << arr[startrow][col] << " ";  
+            count++;
+        }
+        startrow++;
+        
+
+        for (int row = startrow;count <total && row <= endrow; row++)
+        {
+            cout << arr[row][endcoln] << " "; 
+            count++;
+        }
+        endcoln--;
+
+        for (int col = endcoln;count <total && col >= startcoln; col--)
+        {
+            cout << arr[endrow][col] << " "; 
+            count++;
+        }
+        endrow--;
+
+        for (int row = endrow;count <total && row >= startrow; row--)
+        {
+            cout << arr[row][startcoln] << " "; 
+            count++;
+        }
+        startcoln++;
+        
+    }
+}
+
+int main()
+{
+
+    vector<vector<int>> arr = {
+        {1, 2, 3 ,10},
+        {8, 9, 4, 11},
+        {7, 6, 5, 12},
+        {10, 11, 12, 13}
+        };
+
+    spiralform(arr, 4, 4);
+}
